bde61cf841a32eb11102c6aa12cdc58f631b21e7
GiproveR <yaroslav.vjukoff1@mail.ru>
2023-12-16 11:10:11 +0800

    Добавление роли у пользователя

---
+++ backend/app/model/role.js
@@ -0,0 +1,15 @@
+const Sequelize = require('sequelize');
+module.exports = function(sequelize, DataTypes) {
+  return sequelize.define('role', {
+    id: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    name: {
+      type: DataTypes.STRING(50),
+      allowNull: false
+    }
+  });
+};
